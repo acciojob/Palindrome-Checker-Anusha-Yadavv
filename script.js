015de37b4b8e58d05fef86bbe105ad9f1ca0524b
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2024-01-04 23:41:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // complete the given function
 
-function palindrome(str){
+function palindrome(s){
+	 const cleanedString = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+  // Compare the original cleaned string with its reverse
+  return cleanedString === cleanedString.split('').reverse().join('');
 
 }
 module.exports = palindrome
